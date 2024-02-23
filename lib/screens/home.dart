d0b5c02d431f0a1f7454abba2c3e535c0839082f
akashKarmakar02k <akash.anan03@gmail.com>
2024-02-23 22:37:08 +0530

    added single child scroll view and banners

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:country_icons/country_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:major_project/components/top_bar.dart';
+import 'package:major_project/components/tranaudio_banner.dart';
+import 'package:major_project/components/trancode_banner.dart';
 import 'package:major_project/constant.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,118 +10,83 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const TopBar(),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Your Interest",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              SizedBox(
-                height: 150,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: countryList.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 10.0),
-                      child: Container(
-                        width: 75,
-                        height: 75,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.grey[200],
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 55,
-                            horizontal: 15,
-                          ),
-                          child: Container(
-                            child: CountryIcons.getSvgFlag(countryList[index]),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              Column(
+    return MediaQuery.removePadding(
+      context: context,
+      removeTop: true,
+      removeBottom: true,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const TopBar(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.yellow[50],
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(
-                        color: Colors.yellow[200]!,
-                        width: 2,
-                      ),
+                  const Text(
+                    "Your Interest",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Now translate language\nusing your line',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 15,
+                  ),
+                  // Country list
+                  SizedBox(
+                    height: 100,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: countryList.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(right: 10.0),
+                          child: Container(
+                            width: 75,
+                            height: 75,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.grey[200],
                             ),
-                          ),
-                          Text(
-                            'Scan to \nTranscode',
-                            style: TextStyle(
-                              color: Colors.purple[400]!,
-                              fontSize: 20,
-                            ),
-                          ),
-                          ElevatedButton(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.yellow,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 15,
                               ),
-                              child: const Text(
-                                'Try Now',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                ),
-                              ))
-                        ],
-                      ),
+                              child: Container(
+                                child:
+                                    CountryIcons.getSvgFlag(countryList[index]),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
                     ),
                   ),
-                  SizedBox(height: 20),
-                  Container(
-                    width: double.infinity,
-                    height: 70,
-                    decoration: BoxDecoration(
-                      color: Colors.pink[50],
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(
-                        color: Colors.pink[200]!,
-                        width: 2,
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TransCodeBanner(),
+                      SizedBox(
+                        height: 25,
                       ),
-                    ),
-                    child: Text("Hello Bhai"),
-                  ),
+                      Text(
+                        "Activity to join",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TransAudioBanner(),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
