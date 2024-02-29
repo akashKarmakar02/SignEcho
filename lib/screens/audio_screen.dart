94bc0159a49c9128999c23d45aee5213f775d02e
akashKarmakar02k <akash.anan03@gmail.com>
2024-02-29 15:33:33 +0530

    Audio screen layout with bugs

--- lib/screens/audio_screen.dart
+++ lib/screens/audio_screen.dart
@@ -1,3 +1,4 @@
+import 'package:country_icons/country_icons.dart';
 import 'package:flutter/material.dart';
 
 class AudioScreen extends StatelessWidget {
@@ -5,6 +6,66 @@ class AudioScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 24,
+          horizontal: 17,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Select your prefer language",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    border: Border.all(
+                      color: Colors.black54,
+                      width: 1.0,
+                    ),
+                  ),
+                  child: DropdownButton<String>(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                    onChanged: (value) {},
+                    borderRadius: BorderRadius.circular(8),
+                    underline: Container(),
+                    items: <String>[
+                      'Option 1',
+                      'Option 2',
+                      'Option 3',
+                      'Option 4'
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Container(
+                          width: 100,
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text("India"),
+                            ],
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                    hint: const Text('Select an option'),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
